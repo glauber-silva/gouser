b787c7a914a7c2c4c155844ac69e6faf26524cc7
Glauber Silva <glauber.lucio.silva@gmail.com>
2023-11-15 12:39:26 +0000

    add database

---
+++ scripts/databases/run_database.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Connect to the MySQL database and run the SQL command
+# That's not the best way to do it, but it's a way to give more security to the admin user
+mysql -u root -p$MYSQL_ROOT_PASSWORD -D gouserdb -e "
+
+-- Grant all privileges to the new user
+GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;
+
+"
